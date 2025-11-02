0a5b76b3008b3df306a5efd7e426aad94c7efff3
zz10965-alt <zz10965@nyu.edu>
2025-11-02 09:16:11 -0500

    Add fields and constructors to TaskResponse class

--- src/main/java/com/taskforge/dto/TaskResponse.java
+++ src/main/java/com/taskforge/dto/TaskResponse.java
@@ -1,4 +1,36 @@
 package com.taskforge.dto;
 
+import com.taskforge.entity.TaskPriority;
+import com.taskforge.entity.TaskStatus;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
 public class TaskResponse {
+    private long id;
+    private long userId;
+    private String title;
+    private String description;
+    private TaskStatus status;
+    private TaskPriority priority;
+    private LocalDate dueDate;
+    private Boolean aiAssisted;
+
+    //带参数构造函数（给service）
+    public TaskResponse (Long id,Long userId,String title, String description,
+                         TaskStatus status, TaskPriority priority, LocalDate dueDate, Boolean aiAssisted){
+        this.id=id;
+        this.userId=userId;
+        this.title=title;
+        this.description=description;
+        this.status=status;
+        this.priority=priority;
+        this.dueDate=dueDate;
+        this.aiAssisted=aiAssisted;
+    }
+
+    public TaskResponse() {
+
+    }
 }
